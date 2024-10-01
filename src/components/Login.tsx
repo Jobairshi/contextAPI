a74d76009031df5d94bf82a408e60ddda1fed466
Jobair Uddin Jesan <90952065+Jobairshi@users.noreply.github.com>
2024-10-01 16:53:01 +0600

    feat: Add tailwind CSS configuration
    - Added `tailwind.config.js` file to configure Tailwind CSS.
    - Added `@tailwind` directives to `App.css` and `index.css` files.
    - Updated `postcss.config.js` to include Tailwind CSS and Autoprefixer plugins.
    - Updated `vite.config.ts` to use the `@vitejs/plugin-react` plugin.
    - Added login component

---
+++ src/components/Login.tsx
@@ -0,0 +1,69 @@
+import { FormEvent, useState } from "react";
+
+export default function Login() {
+
+
+    const [username, setUsername]  = useState('');
+    const [password, setPassword] = useState('');
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formdata = new FormData(event.currentTarget);
+    const name = formdata.get("username");
+    const pass = formdata.get("password");
+    setUsername(username);
+    setPassword(password);
+    console.log(name, " : : ", pass);
+  }
+
+  return (
+    <div className="my-10 container m-auto bg-slate-400 p-10 rounded-2xl shadow-2xl shadow-yellow-300">
+      <div>
+        <form
+          className="flex flex-col justify-center gap-2"
+          action=""
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-col gap-2 p-2 justify-start">
+            <label htmlFor="user-name" className="">
+              user-name
+            </label>
+            <input
+              className="border-2 rounded-lg p-2 px-4 border-yellow-300"
+              name="username"
+              placeholder="user-name"
+              type="text"
+              required
+              minLength={3}
+              maxLength={10}
+              title="Username should be 3-10 characters long and contain only alphanumeric characters."
+              pattern="[A-Za-z0-9]+"
+            />
+          </div>
+
+          <div className="flex flex-col gap-2 p-2 justify-start">
+            <label htmlFor="password" className="">
+              password
+            </label>
+            <input
+              className="border-2 rounded-lg p-2 px-4 border-yellow-300"
+              type="password"
+              name="password"
+              placeholder="input password"
+              required
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}"
+              title="password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character."
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="font-bold w-[30%] h-10 bg-yellow-200 border-2 rounded-lg m-auto hover:bg-yellow-300"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
